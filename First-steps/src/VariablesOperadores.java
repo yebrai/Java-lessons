6e2c5adce8807b9e3d5c50f5f13bdf4f9b5be449
yebrai <ivansevv@gmail.com>
2023-04-22 19:02:36 +0200

    new lessons

---
+++ First-steps/src/VariablesOperadores.java
@@ -0,0 +1,29 @@
+public class VariablesOperadores {
+    public static void main (String[] args) {
+        byte age = 28;
+        System.out.println(age);
+        age = 31;
+        System.out.println(age);
+        
+        boolean developer = true;
+        char name = 'l';
+        double cm = 6;
+        double pulgada = 2.54;
+
+        double resultado = cm/pulgada;
+
+        // Constantes se declaran con final
+
+        final boolean java = true;
+
+        //Operadores iguales que en JS
+
+        System.out.println("En " + cm + " cm hay" + resultado + " pulgadas");
+
+
+
+        
+
+
+    }
+}
