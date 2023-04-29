f8bd23faedaa4c8cfebf47e87dd0d1f8260084df
yebrai <ivansevv@gmail.com>
2023-04-29 12:53:43 +0200

    Cadenas y manipulacion en Java

---
+++ First-steps/src/Cadenas.java
@@ -0,0 +1,18 @@
+public class Cadenas {
+    public static void main (String[] args) {
+
+        String miNombre= "Ivan";
+
+        System.out.println(miNombre);
+
+        System.out.println(miNombre + " tiene " + miNombre.length() + " caracteres" + " y su primera letra es " + miNombre.charAt(0));
+
+        String cadenaString = "esto es un texto de prueba";
+
+        System.out.println(cadenaString.substring(5, 16));
+
+        System.out.println(miNombre.equals(cadenaString));
+
+
+    }
+}
