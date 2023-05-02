d23cd9e7286e99c43e28905bbec8736c7efbd587
yebrai <ivansevv@gmail.com>
2023-05-02 15:45:14 +0200

    Java arrays

---
+++ First-steps/src/useArray.java
@@ -0,0 +1,12 @@
+public class useArray {
+    public static void main(String[] args) {
+        
+        int [] mi_matriz= new int[5];
+
+        mi_matriz[0] = 22;
+        mi_matriz[1] = 24;
+
+        int [] mi_matriz2={22,24,12,10,5};
+
+    }
+}
